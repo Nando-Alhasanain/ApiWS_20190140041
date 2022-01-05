2ea665fed835579642acaa25c26e24b3001bb9ef
Nando Alhasanain <Nandoalhasanain@gmail.com>
2022-01-05 21:23:46 +0700

    add database

---
+++ src/main/java/ti/webservice/project/dua/Barang.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ti.webservice.project.dua;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Nando
+ */
+@Entity
+@Table(name = "barang")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Barang.findAll", query = "SELECT b FROM Barang b"),
+    @NamedQuery(name = "Barang.findByIdBarang", query = "SELECT b FROM Barang b WHERE b.idBarang = :idBarang"),
+    @NamedQuery(name = "Barang.findByNamaBarang", query = "SELECT b FROM Barang b WHERE b.namaBarang = :namaBarang"),
+    @NamedQuery(name = "Barang.findByJumlah", query = "SELECT b FROM Barang b WHERE b.jumlah = :jumlah"),
+    @NamedQuery(name = "Barang.findByHarga", query = "SELECT b FROM Barang b WHERE b.harga = :harga")})
+public class Barang implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id_barang")
+    private Integer idBarang;
+    @Column(name = "nama_barang")
+    private String namaBarang;
+    @Column(name = "jumlah")
+    private Integer jumlah;
+    @Column(name = "harga")
+    private Integer harga;
+
+    public Barang() {
+    }
+
+    public Barang(Integer idBarang) {
+        this.idBarang = idBarang;
+    }
+
+    public Integer getIdBarang() {
+        return idBarang;
+    }
+
+    public void setIdBarang(Integer idBarang) {
+        this.idBarang = idBarang;
+    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
+    public Integer getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(Integer jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public Integer getHarga() {
+        return harga;
+    }
+
+    public void setHarga(Integer harga) {
+        this.harga = harga;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idBarang != null ? idBarang.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Barang)) {
+            return false;
+        }
+        Barang other = (Barang) object;
+        if ((this.idBarang == null && other.idBarang != null) || (this.idBarang != null && !this.idBarang.equals(other.idBarang))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ti.webservice.project.dua.Barang[ idBarang=" + idBarang + " ]";
+    }
+    
+}
